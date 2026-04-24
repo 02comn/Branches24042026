c8d369c4164c979f50cabdbf40690266836964a2
Leonardo de Jesus Santos <pedro.m.fonseca@aluno.senai.br>
2026-04-24 09:15:10 -0300

    Modificação no arquivo Program.cs
    Letras da música "Hello, World"

--- Projeto/Projeto/Projeto/Program.cs
+++ Projeto/Projeto/Projeto/Program.cs
@@ -3,5 +3,27 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine(
+
+            "Programmed to work and not to feel" +
+            "\r\nNot even sure that this is real" +
+            "\r\nHello, world" +
+
+            "\r\nFind my voice" +
+            "\r\nAlthough, it sounds like bits and bytes" +
+            "\r\nMy circuitry is filled with mites" +
+            "\r\nHello, world" +
+
+            "\r\nOh, will I find a love" +
+            "\r\nOr a power plug?" +
+            "\r\nOh, digitally isolated" +
+            "\r\nOh, creator, please don't leave me waiting" +
+
+            "\r\nHello, world" +
+            "\r\nProgrammed to work and not to feel" +
+            "\r\nNot even sure that this is real" +
+            "\r\nHello, world"
+            
+            );
     }
 }
